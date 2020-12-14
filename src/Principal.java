4a056def4b35bdc2c0dc9c4fef2a31af2d23c0ca
JAVIER LEGUA <javierlegua14@gmail.com>
2020-12-14 11:51:04 +0100

    comienzo con poo

---
+++ src/Principal.java
@@ -0,0 +1,16 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Gato minusi = new Gato("Minusi", 7, 4);
+		Gato gato1 = new Gato("juan");
+		Gato gato2 = new Gato();
+		Perro pe = new Perro("Leon");
+		System.out.println(pe.toString());
+		
+	}
+
+	}
+
+}
