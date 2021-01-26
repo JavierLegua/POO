6d0be87ba858990eee356818eece362aeaa10d74
Javier Legua <javierlegua14@gmail.com>
2021-01-26 22:58:52 +0100

    Metodos creados a falta de correccion

--- src/GestionBebidas/Almacen.java
+++ src/GestionBebidas/Almacen.java
@@ -13,8 +13,10 @@ public class Almacen {
 		this.mEstanteria = new Bebidas[5][5];
 		Azucarada c = new Azucarada("2", 2, 4, "Fanta", 15, true);
 		Azucarada a = new Azucarada("3", 5, 9, "Fanta naranja", 17, false);
+		Azucarada s = new Azucarada("5", 5, 9, "Fanta limon", 17, false);
 		this.mEstanteria[0][0] = c;
 		this.mEstanteria[2][2] = a;
+		this.mEstanteria[2][3] = s;
 	}
 	
 	public float calcularPrecioTotal() {
@@ -29,6 +31,65 @@ public class Almacen {
 		}
 		return precioTotal;
 	}
+	//No funciona
+	public float calcularPrecioTotalMarca() {
+		float precioTotal = 0;
+		
+		for (Bebidas[] bebidas : mEstanteria) {
+			for (Bebidas bebida : bebidas) {
+				if (bebida != null && bebida.getMarca().equals("Marca deseada.")) {
+					precioTotal += bebida.getPrecio();
+				}
+			}
+		}
+		return precioTotal;
+	}
+	//Preguntar sobre este proceso
+	public float calcularPrecioEstanteria() {
+		float precioTotal = 0;
+		
+		for (Bebidas[] bebidas : mEstanteria) {
+			for (Bebidas bebida : bebidas) {
+				if (bebida != null) {
+					precioTotal += bebida.getPrecio();
+				}
+			}
+		}
+		
+		return precioTotal;
+	}
+	
+	public void añadirProducto() {
+		Azucarada d = new Azucarada("5", 4, 1.1, "Fanta", 56, true);
+		String codigo = d.getId();
+		boolean aux = false, aux1 = false;
+		
+		for (Bebidas[] bebidas : mEstanteria) {
+			for (Bebidas bebida : bebidas) {
+				if (bebida != null && bebida.getId().equalsIgnoreCase(codigo)) {
+					aux = true;
+					System.out.println("Tu producto esta disponible.");
+					break;
+				}
+			}
+		}
+		if (aux == false) {
+			for (Bebidas[] bebidas : mEstanteria) {
+				for (Bebidas bebida : bebidas) {
+					if (bebida == null) {
+						bebida = d;
+						System.out.println("Tu producto ha sido añadido.");
+						aux1 = true;
+						break;
+					}
+				}
+				if (aux1 == true) {
+					break;
+				}
+			}
+		}
+	}
+	
 
 	public String getNombre() {
 		return nombre;
@@ -42,5 +103,7 @@ public class Almacen {
 	public String toString() {
 		return "Almacen [nombre=" + nombre + ", mEstanteria=" + Arrays.toString(mEstanteria) + "]";
 	}
+
+	
 
 }
--- src/GestionBebidas/Main.java
+++ src/GestionBebidas/Main.java
@@ -5,9 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Almacen c = new Almacen("Javier legua");
+		Azucarada z = new Azucarada("3", 4, 5, "coca cola", 2, true);
 		
-		System.out.println(c.calcularPrecioTotal());
-
+		System.out.println(z.toString());
+		//Calcular precio estanteria funciona
+		//Calcular precio marca no funciona
+		//Calcular precio total funciona
+		//Añadir producto no funciona
 	}
 
 }
